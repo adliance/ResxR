ee6b22a948b48b61b763ee75bfea7906bee693fe
Adrian Fürschuß <adrian.fuerschuss@adliance.net>
2023-09-29 19:44:52 +0200

    Add format and build cmds.

---
+++ build.cmd
@@ -0,0 +1,19 @@
+dotnet format ResxDiff.sln -v normal
+
+if %ERRORLEVEL% NEQ 0 (
+   echo [91mFormat failed[0m %errorlevel%
+   exit /b %errorlevel%
+)
+
+dotnet build ResxDiff.sln -warnAsError
+
+if %ERRORLEVEL% NEQ 0 (
+   echo [91mBuild failed[0m %errorlevel%
+   exit /b %errorlevel%
+)
+
+dotnet test ResxDiff.sln
+
+if %ERRORLEVEL% EQU 0 (
+   echo [92mBuild succeeded[0m %errorlevel%
+)
---
+++ format.cmd
@@ -0,0 +1,1 @@
+dotnet format ResxDiff.sln -v normal
