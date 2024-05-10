bc6ff4b09ee203afcf519636ad98ab06b1700fba
afuersch <adrian.fuerschuss@gmx.net>
2024-05-10 11:20:03 +0200

    Fix Typo in install cmd.

--- install.cmd
+++ install.cmd
@@ -1,5 +1,5 @@
 dotnet pack .\src\ResxDiffConsole\ResxDiffConsole.csproj -c Release
 
-@ECHO Updating your local TogglR tool to the latest version ...
+@ECHO Updating your local ResxDiff tool to the latest version ...
 dotnet tool install --global --add-source .\src\ResxDiffConsole\nupkg\ ResxDiff --no-cache --ignore-failed-sources -v q
 dotnet tool update --global --add-source .\src\ResxDiffConsole\nupkg\ ResxDiff --no-cache --ignore-failed-sources -v q
